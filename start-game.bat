76043d946b26fc1156c2c5e362a105bf8049555d
ndokutovich <ndokutovich@gmail.com>
2025-10-05 20:26:34 +0300

    refactor: Update start-game.bat for improved server setup
    - Commented out the previous Python server command and replaced it with a new command to set the PORT environment variable.
    - Introduced a new command to serve the application, enhancing the startup process for the game.
    
    These changes streamline the game startup process and ensure proper server configuration.

--- start-game.bat
+++ start-game.bat
@@ -4,4 +4,6 @@ echo.
 echo The game will open at: http://localhost:3005
 echo Press Ctrl+C to stop the server
 echo.
-python -m http.server 3005
+rem python -m http.server 3005
+set PORT=3005
+serve .
